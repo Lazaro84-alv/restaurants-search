2a81cd56e722865492469e45665faef31405e78b
Lázaro Alves <geo.eolo@gmail.com>
2021-06-25 00:50:57 -0300

    Desenvolve o carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import Slider from "react-slick";
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true
+  };
 
   return (
     <Wrapper>
@@ -23,6 +35,14 @@ const Home = () => {
               onChange={(e) => setInputValue(e.target.value)} 
               />
             </TextField>
+            <CarouselTitle>Na sua Área</CarouselTitle>
+            <Slider { ...settings}>
+              <Card photo={restaurante} />
+              <Card photo={restaurante} />
+              <Card photo={restaurante} />
+              <Card photo={restaurante} />
+              <Card photo={restaurante} />
+            </Slider>
         </Search>
       </Container>
       <Map />
