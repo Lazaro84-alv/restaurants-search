41b81c9b83d1a265cb1e772037148423ece911cf
Lázaro Alves <geo.eolo@gmail.com>
2021-06-24 21:40:33 -0300

    adiciona styled ao conteúdo lateral

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello World!</Container>;
+import logo from '../../assets/logo.svg';
+
+import { Container, Search, Logo, Wrapper, Map } from './styles';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do restaurante" />
+          <TextField
+              label='Pesquisar Restaurantes'
+              outlined
+              trailingIcon={<MaterialIcon role="button" icon="search"/>}>
+              <Input
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)} 
+              />
+            </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
